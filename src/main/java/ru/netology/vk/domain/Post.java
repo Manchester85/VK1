3efb436e991554d7c342ef70599183fc2c0f6c66
Maria Vostrikova <vostrikova.maria2017@yandex.ru>
2021-09-25 21:58:29 +0300

    v1.0

---
+++ src/main/java/ru/netology/vk/domain/Post.java
@@ -0,0 +1,45 @@
+package ru.netology.vk.domain;
+
+public class Post {
+    private int id;
+    private String logoUrl;
+    private String Header;
+    private String text;
+    private String imageUrl;
+    private String info;
+    private boolean read;
+    private int like;
+    private int share;
+    private int view;
+    private String comments;
+    private int time;
+    private int date;
+
+
+
+/*public void setDate(int date) {
+    if (date ==  ) {
+
+    }
+
+    this.date = date;
+}
+
+public int getDate() {
+    return date;
+}
+public void setTime(int time) {
+    if (time ==  ) {
+
+    }
+
+    this.time = time;
+}
+
+public int getTime() {
+    return time;
+}
+
+*/
+
+}
