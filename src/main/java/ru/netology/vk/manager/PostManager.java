a603c091cc83c1ebf4b48fe6663ccfb041f82ec7
Maria Vostrikova <vostrikova.maria2017@yandex.ru>
2021-09-26 09:31:52 +0300

    PostManager added

---
+++ src/main/java/ru/netology/vk/manager/PostManager.java
@@ -0,0 +1,22 @@
+package ru.netology.vk.manager;
+
+
+import ru.netology.vk.domain.Post;
+
+public class PostManager {
+    private ru.netology.vk.domain.Post[] posts;
+
+    public Post[] getPostsForFeed() {
+        return null;
+    }
+
+    public Post[] search(int ownerId, String domain, String query, int ownersOnly, int count, int offset) {
+
+
+        return null;
+    }
+
+    public void delete(int ownersId, int postId) {
+
+    }
+}
